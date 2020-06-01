c66e5219aa5bb4be3034955d566e33473612c93e
Rainier Devolder <devolde2@msu.edu>
2020-06-01 00:45:16 -0400

    actor profile enum additions

--- scripts/npc/shared/enums/shActorProfileEnum.lua
+++ scripts/npc/shared/enums/shActorProfileEnum.lua
@@ -1,16 +1,25 @@
 class "ActorProfileEnum"
 
 function ActorProfileEnum:__init()
-    self.PanauMilitaryPatrollerBasic = 1
+    self.Patroller = 1
 
     
     self.descriptions = {
-        [self.PanauMilitaryPatrollerBasic] = "Panau Military Patroller Basic"
+        [self.Patroller] = "Patroller"
     }
 end
 
 function ActorProfileEnum:GetDescription(actor_profile_enum)
+    assert(self.descriptions[actor_profile_enum] ~= nil)
     return self.descriptions[actor_profile_enum]
+end
+
+function ActorProfileEnum:GetClass(actor_profile_enum)
+    local actor_profiles = {
+        [self.Patroller] = Patroller
+    }
+    assert(actor_profiles[actor_profile_enum] ~= nil)
+    return actor_profiles[actor_profile_enum]
 end
 
 ActorProfileEnum = ActorProfileEnum()
