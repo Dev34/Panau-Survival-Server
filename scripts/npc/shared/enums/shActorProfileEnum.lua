7eb1dc933edab8578a3263395210eda368c49126
Rainier Devolder <devolde2@msu.edu>
2020-06-11 22:54:52 -0400

    add GetModelId for ActorProfileEnum

--- scripts/npc/shared/enums/shActorProfileEnum.lua
+++ scripts/npc/shared/enums/shActorProfileEnum.lua
@@ -7,6 +7,10 @@ function ActorProfileEnum:__init()
     self.descriptions = {
         [self.Patroller] = "Patroller"
     }
+
+    self.model_ids = {
+        [self.Patroller] = 66
+    }
 end
 
 function ActorProfileEnum:GetDescription(actor_profile_enum)
@@ -20,6 +24,11 @@ function ActorProfileEnum:GetClass(actor_profile_enum)
     }
     assert(actor_profiles[actor_profile_enum] ~= nil)
     return actor_profiles[actor_profile_enum]
+end
+
+function ActorProfileEnum:GetModelId(actor_profile_enum)
+    assert(self.model_ids[actor_profile_enum] ~= nil, "Actor profile enum " .. tostring(actor_profile_enum) .. " is not mapped to a model id")
+    return self.model_ids[actor_profile_enum]
 end
 
 ActorProfileEnum = ActorProfileEnum()
