e15c2e8d3bb8a4f476bde576fcc6e4a69c078ceb
Rainier Devolder <devolde2@msu.edu>
2020-06-20 00:30:53 -0400

    make server-side patroller actor profile use NavigatePathBehavior

--- scripts/npc/server/sPatroller.lua
+++ scripts/npc/server/sPatroller.lua
@@ -5,12 +5,19 @@ function Patroller:__init()
     getter_setter(self, "path")
     self.actor = Actor()
     self.actor:SetActorProfileEnum(ActorProfileEnum.Patroller)
+    self.path_speed_multiplier = 1.03
+
+    -- Configure behaviors
+    self.actor:UseBehavior(self, NavigatePathBehavior)
+    self.actor.behaviors.NavigatePathBehavior:SetSpeedMultiplier(self.path_speed_multiplier)
 end
 
 function Patroller:GetSyncData()
     local sync_data = {}
 
     sync_data.path = self.path:GetJsonCompatibleData()
+    sync_data.path_progress = self.actor.behaviors.NavigatePathBehavior:GetProgress() or 0.0001
+    sync_data.path_speed_multiplier = self.path_speed_multiplier
     
     return sync_data
 end
@@ -21,6 +28,23 @@ function Patroller:InitializeFromSpawnPoint(spawn_point)
     self.actor.position = Copy(self.path:GetPositions()[1])
     if self.actor.position then
         self.actor.cell = GetCell(self.actor.position, ActorSync.cell_size)
+    end
+    self.actor.behaviors.NavigatePathBehavior:SetPath(self.path)
+    self.actor.behaviors.NavigatePathBehavior:StartPath()
+end
+
+-- "PathFinished" behavior event handler
+-- could be moved to a separate behavior, but it's not much code
+function Patroller:PathFinished()
+    print("Entered PathFinished handler")
+    -- reverse the path and start again
+    self.path = self.path:GetReversedCopy()
+    self.actor.behaviors.NavigatePathBehavior:SetPath(self.path)
+    self.actor.behaviors.NavigatePathBehavior:StartPath()
+    if count_table(self.actor.streamed_players) > 0 then
+        Network:SendToPlayers(self.actor:GetStreamedPlayersSequential(), "npc/NextPath" .. tostring(self.actor:GetActorId()), {
+            path = self.path:GetJsonCompatibleData()
+        })
     end
 end
 
