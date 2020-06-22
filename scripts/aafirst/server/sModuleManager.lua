5d188626ce0fda0e66f20cbebae65b75e6c5e350
Rainier Devolder <devolde2@msu.edu>
2020-06-22 11:24:24 -0400

    fix merge conflict

--- scripts/aafirst/server/sModuleManager.lua
+++ scripts/aafirst/server/sModuleManager.lua
@@ -2,4 +2,7 @@
 
 Events:Subscribe("ServerStart", function()
     Console:Run("unload settings")
+    Console:Run("unload survival") -- TODO: remove this
+    --Console:Run("unload terrainmap")
+    Console:Run("unload effects")
 end)
