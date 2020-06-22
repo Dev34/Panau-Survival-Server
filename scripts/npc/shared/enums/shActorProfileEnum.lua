6d6848f3d9ad3ae40273901e60f1006161006109
Rainier Devolder <devolde2@msu.edu>
2020-06-22 11:48:54 -0400

    define CityLooter actor profile & random model selection for actor profile

--- scripts/npc/shared/enums/shActorProfileEnum.lua
+++ scripts/npc/shared/enums/shActorProfileEnum.lua
@@ -2,14 +2,20 @@ class "ActorProfileEnum"
 
 function ActorProfileEnum:__init()
     self.Patroller = 1
+    self.CityLooter = 2
 
     
     self.descriptions = {
-        [self.Patroller] = "Patroller"
+        [self.Patroller] = "Patroller",
+        [self.CityLooter] = "CityLooter"
     }
 
+    -- https://wiki.jc-mp.com/Character_Models
     self.model_ids = {
-        [self.Patroller] = 66
+        [self.Patroller] = 66,
+        [self.CityLooter] = {
+            94, 92, 76, 72, 69, 68, 56, 50
+        }
     }
 end
 
@@ -20,15 +26,21 @@ end
 
 function ActorProfileEnum:GetClass(actor_profile_enum)
     local actor_profiles = {
-        [self.Patroller] = Patroller
+        [self.Patroller] = Patroller,
+        [self.CityLooter] = CityLooter
     }
-    assert(actor_profiles[actor_profile_enum] ~= nil)
+    assert(actor_profiles[actor_profile_enum] ~= nil, "Actor profile enum does not have a mapped class")
     return actor_profiles[actor_profile_enum]
 end
 
 function ActorProfileEnum:GetModelId(actor_profile_enum)
     assert(self.model_ids[actor_profile_enum] ~= nil, "Actor profile enum " .. tostring(actor_profile_enum) .. " is not mapped to a model id")
-    return self.model_ids[actor_profile_enum]
+    local model_data = self.model_ids[actor_profile_enum]
+    if type(model_data) == "table" then
+        return random_table_value(model_data)
+    else
+        return model_data
+    end
 end
 
 ActorProfileEnum = ActorProfileEnum()
