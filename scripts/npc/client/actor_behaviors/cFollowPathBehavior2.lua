3b2962994db4c7563cc977b97c9dfb3700ed293a
Rainier Devolder <devolde2@msu.edu>
2020-09-23 22:25:22 -0400

    pausing & fixes for FollowPathBehavior

--- scripts/npc/client/actor_behaviors/cFollowPathBehavior2.lua
+++ scripts/npc/client/actor_behaviors/cFollowPathBehavior2.lua
@@ -6,6 +6,7 @@ function FollowPathBehavior2:__init(actor_profile_instance)
     self.actor_profile_instance = actor_profile_instance
     getter_setter(self, "active")
     getter_setter(self, "path_progress")
+    self.max_distance_from_correct_position = 15
 
     self.moving = false
     self.turning = false
@@ -14,6 +15,7 @@ function FollowPathBehavior2:__init(actor_profile_instance)
     if IsTest then
         self.render_debug_sub = Events:Subscribe("Render", self, self.RenderDebug)
     end
+
     self.next_path_sub = Network:Subscribe("npc/NextPath" .. tostring(self.actor_profile_instance.actor.actor_id), self, self.NextPath)
     self:StartUpdateThread()
 end
@@ -68,17 +70,22 @@ end
 function FollowPathBehavior2:NextPath(args)
     self.path = Path()
     self.path:InitializeFromJsonData(args.path)
+    self.actor_profile_instance:SetPath(self.path)
 
     local ang = Angle(Angle.FromVectors(Vector3.Forward, self.path.positions[2] - self.path.positions[1]).yaw, 0, 0)
 
     if not IsValid(self.actor_profile_instance.client_actor) then
-        self.actor_profile_instance:Respawn(self.path.positions[1], ang)
+        if self.actor_profile_instance.actor:GetClientActorSpawnedTime() > 3.5 then
+            Chat:Print("Respawned actor at initial path node because ClientActor became invalid", Color.Red)
+            self.actor_profile_instance:Respawn(self.path.positions[1], ang)
+        end
     else
         -- this looks fine, no need to do fancy turning simulation or running to this position
         self.actor_profile_instance.client_actor:SetPosition(self.path.positions[1])
         self.actor_profile_instance.client_actor:SetAngle(ang)
+        self.actor_profile_instance.client_actor:SetBaseState(AnimationState.SUprightBasicNavigation)
     end
-    self:FollowNewPath(self.path, 0.00001, self.current_path_speed_multiplier)
+    self:FollowNewPath(self.path, 0.00001, args.path_speed_multiplier)
     self.moving = false
 end
 
@@ -89,7 +96,10 @@ function FollowPathBehavior2:EnsureIsMoving()
     end
 end
 
+-- high-level frequent update function for pathing
 function FollowPathBehavior2:ContinuePathing()
+    if self.paused then return end
+
     self:EnsureIsMoving()
     self:CheckIfShouldTakeCorrectiveAction()
 end
@@ -104,6 +114,9 @@ function FollowPathBehavior2:CheckIfShouldTakeCorrectiveAction()
     
     -- TODO: if left_right > threshold then turn client actor so they're heading in the right direction after being displaced
     local forward = -(self.client_actor_position - self.correct_position):Dot(Angle(self.client_actor_angle.yaw, 0, 0) * Vector3.Backward) -- positive if in-front, negative if behind
+
+    local distance_from_correct_position = Vector3.Distance(self.client_actor_position, self.correct_position)
+    -- TODO: check if distance_from_correct_position > self.max_distance_from_correct_position and teleport if too far
     
     if forward < -5 and not self.speed_modding then
         self.speed_modding = true
@@ -117,6 +130,7 @@ function FollowPathBehavior2:CheckIfShouldTakeCorrectiveAction()
                 ms = timer:GetMilliseconds()
 
                 if IsValid(self.actor_profile_instance.client_actor) then
+                    if self.paused then break end
                     self.actor_profile_instance.client_actor:SetInput(Action.MoveForward, 0.35)
                     -- base state 19 is super fast sprint
                 end
@@ -124,7 +138,9 @@ function FollowPathBehavior2:CheckIfShouldTakeCorrectiveAction()
                 Timer.Sleep(1)
             end
 
-            self.actor_profile_instance.client_actor:SetBaseState(7) -- this needs to happen to ensure we maintain regular speed
+            if not self.paused then
+                self.actor_profile_instance.client_actor:SetBaseState(7) -- this needs to happen to ensure we maintain regular speed
+            end
             self.speed_modding = false
         end)()
     end
@@ -143,6 +159,35 @@ function FollowPathBehavior2:NextNode(node_index)
         end
     end
     --Chat:Print("Next Node", Color.Blue)
+end
+
+function FollowPathBehavior2:PausePath()
+    if self.path_navigation then
+        self.path_navigation:Pause()
+    end
+    if self.actor_profile_instance.client_actor and IsValid(self.actor_profile_instance.client_actor) then
+        self.actor_profile_instance.client_actor:SetBaseState(6) -- idle
+        self.moving = false
+    end
+
+    self.paused = true
+end
+
+function FollowPathBehavior2:ResumePath()
+    self.paused = false
+
+    -- turn to next node
+    self:TurnToCurrentNode()
+    if self.path_navigation then
+        self.path_navigation:Resume()
+    end
+end
+
+function FollowPathBehavior2:TurnToCurrentNode()
+    if self.path.positions[self.current_node_index + 1] then
+        local ang = Angle.FromVectors(Vector3.Forward, self.path.positions[self.current_node_index + 1] - self.actor_profile_instance.client_actor:GetPosition())
+        self:TurnToAngle(ang)
+    end
 end
 
 function FollowPathBehavior2:TurnToAngle(angle) -- yaw, 0, 0
@@ -172,6 +217,8 @@ function FollowPathBehavior2:TurnToAngle(angle) -- yaw, 0, 0
                 if yaw_dif > .02 then
                     local new_angle = Angle.Slerp(client_actor_angle_safe, angle, .085)
                     if new_angle then
+                        new_angle.roll = 0
+                        new_angle.pitch = 0
                         self.actor_profile_instance.client_actor:SetAngle(new_angle)
                         --Chat:Print("TURNING!", Color.Red)
                     else
