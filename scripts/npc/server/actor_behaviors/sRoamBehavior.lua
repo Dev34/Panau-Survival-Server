6c1481c6495363ef4422fda8546577c968882803
Rainier Devolder <devolde2@msu.edu>
2020-09-23 22:26:46 -0400

    decouple RoamBehavior with PathNavigation

--- scripts/npc/server/actor_behaviors/sRoamBehavior.lua
+++ scripts/npc/server/actor_behaviors/sRoamBehavior.lua
@@ -4,10 +4,10 @@ RoamBehavior.name = "RoamBehavior"
 function RoamBehavior:__init(actor_profile_instance)
     self.actor_profile_instance = actor_profile_instance
     getter_setter(self, "active")
-    
 end
 
-function RoamBehavior:FindNextPath()-- requires GetPosition on the actor profile to be accurate at this point in time
+function RoamBehavior:FindNextPath() -- requires GetPosition on the actor profile to be accurate at this point in time
+    Chat:Broadcast("Started Roaming", Color.Green)
     local current_position = self.actor_profile_instance.actor:GetPosition()
     PathEngineManager:GetRoamPath(current_position, self.PathRequestCallback, self)
 end
@@ -18,19 +18,4 @@ function RoamBehavior:PathRequestCallback(data)
     else
         self.actor_profile_instance.actor:FireBehaviorEvent("PathAcquired", data.path)
     end
-end
-
-function RoamBehavior:GetPosition()
-    if not self.path_navigation then return nil end
-    return self.path_navigation:GetPosition()
-end
-
--- returns 0.0 -> 1.0 path progress
-function RoamBehavior:GetProgress()
-    if not self.path_navigation then return nil end
-    return self.path_navigation:GetPathProgress()
-end
-
-function RoamBehavior:PathFinishedCallback()
-    self.actor_profile_instance.actor:FireBehaviorEvent("PathFinished")
 end
