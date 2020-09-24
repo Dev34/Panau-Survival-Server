d3eaf52ef5aa6619859c83e7f02ade14a71d947b
Rainier Devolder <devolde2@msu.edu>
2020-09-23 22:29:38 -0400

    functional CityLooter with behaviors implemented

--- scripts/npc/client/cCityLooter.lua
+++ scripts/npc/client/cCityLooter.lua
@@ -4,13 +4,18 @@ function CityLooter:__init()
     getter_setter(self, "actor")
     getter_setter(self, "path")
     self.actor = Actor()
-    self.actor:SetActorProfileEnum(ActorProfileEnum.CityLooter)
-    self.actor:SetModelId(ActorProfileEnum:GetModelId(ActorProfileEnum.CityLooter))
 end
 
 function CityLooter:InitializeBehaviors()
     -- Configure behaviors
+    self.actor:UseBehavior(self, DetectLocalPlayerHitsBehavior)
     self.actor:UseBehavior(self, FollowPathBehavior2)
+    self.actor:UseBehavior(self, ShootTargetBehavior)
+    self.actor:UseBehavior(self, ChasePlayerBehavior)
+
+    self.actor:SubscribeToStateEvent("ShootTarget", self.actor.behaviors.ShootTargetBehavior, self.actor.behaviors.ShootTargetBehavior.ShootTarget)
+    self.actor:SubscribeToStateEvent("PausePath", self.actor.behaviors.FollowPathBehavior2, self.actor.behaviors.FollowPathBehavior2.PausePath)
+    self.actor:SubscribeToStateEvent("ResumePath", self.actor.behaviors.FollowPathBehavior2, self.actor.behaviors.FollowPathBehavior2.ResumePath)
 end
 
 function CityLooter:GetSyncData()
@@ -37,19 +42,43 @@ function CityLooter:Spawn()
         local next_path_node = self.path.positions[self.actor.behaviors.FollowPathBehavior2.current_node_index + 1]
         local spawn_angle = next_path_node and Angle(Angle.FromVectors(Vector3.Forward, next_path_node - spawn_node).yaw, 0, 0) or Angle()
         local spawn_position = self.actor.behaviors.FollowPathBehavior2:GetPosition()
-        self.client_actor = ClientActor.Create(1, {
+        self.client_actor = ClientActor.Create(self, {
             model_id = self.actor:GetModelId(),
             position = spawn_position,
             angle = spawn_angle
         })
     else
-        self.client_actor = ClientActor.Create(1, {
+        self.client_actor = ClientActor.Create(self, {
             model_id = self.actor:GetModelId(),
             position = self.actor:GetPosition(),
             angle = Angle()
         })
     end
+    Timer.SetTimeout(1500, function()
+        if self.actor and self.actor.active and self.client_actor and IsValid(self.client_actor) and self.actor.weapon_enum then
+            self.client_actor:GiveWeapon(1, Weapon(WeaponEnum:GetWeaponId(self.actor.weapon_enum), 999999, 999999))
+        end
+    end)
     self.actor:Respawned()
+end
+
+-- LocalPlayer shot the actor
+function CityLooter:LocalPlayerHit()
+    -- if LoS then shoot behavior
+    -- else following behavior
+    --Chat:Print("CityLooter:LocalPlayerHit", Color.Red)
+    Network:Send("npc/PlayerAttackNPC" .. tostring(self.actor.actor_id), {
+        line_of_sight = self:HasLineOfSightOnLocalPlayer()
+    })
+end
+
+function CityLooter:HasLineOfSightOnLocalPlayer()
+    if not IsValid(self.client_actor) then return false end
+    local client_actor_pos = self.client_actor:GetPosition() + Vector3(0, 2.3, 0)
+    local localplayer_pos = LocalPlayer:GetBonePosition("ragdoll_Head")
+    
+	local ray = Physics:Raycast(client_actor_pos, Angle.FromVectors(Vector3.Forward, localplayer_pos - client_actor_pos) * Vector3.Forward, 0, 850, false)
+	return (ray.entity and ray.entity.__type == "LocalPlayer")
 end
 
 function CityLooter:Respawn(pos, ang)
@@ -59,7 +88,7 @@ function CityLooter:Respawn(pos, ang)
     print("self:")
     print(self)
     print(self.actor)
-    self.client_actor = ClientActor.Create(1, {
+    self.client_actor = ClientActor.Create(self, {
         model_id = self.actor:GetModelId(),
         position = pos,
         angle = ang
--- scripts/npc/server/sCityLooter.lua
+++ scripts/npc/server/sCityLooter.lua
@@ -1,17 +1,46 @@
 class "CityLooter"
 
-function CityLooter:__init()
+function CityLooter:__init(actor_id)
     getter_setter(self, "actor")
     getter_setter(self, "path")
     getter_setter(self, "announced")
-    self.actor = Actor()
+    getter_setter(self, "target")
+    self.actor = Actor(actor_id)
     self.actor:SetActorProfileEnum(ActorProfileEnum.CityLooter)
     self.path_speed_multiplier = 1.03
+    self.actor:SetWeaponEnum(random_weighted_table_value({
+        [WeaponEnum.Handgun] = 120,
+        [WeaponEnum.Revolver] = 120,
+        [WeaponEnum.SawnOffShotgun] = 30,
+        [WeaponEnum.SMG] = 30,
+        [WeaponEnum.Assault] = 25,
+        [WeaponEnum.MachineGun] = 25,
+        [WeaponEnum.Sniper] = 15
+    }))
+    self.actor:SetModelId(random_weighted_table_value({
+        [94] = 10,
+        [92] = 10,
+        [76] = 10,
+        [72] = 10,
+        [69] = 10,
+        [68] = 10,
+        [56] = 10,
+        [50] = 10
+    }))
 
     -- Configure behaviors
     self.actor:UseBehavior(self, NavigatePathBehavior)
     self.actor:UseBehavior(self, RoamBehavior)
+    self.actor:UseBehavior(self, ShootTargetBehavior)
+    self.actor:UseBehavior(self, ChasePlayerBehavior)
     self.actor.behaviors.NavigatePathBehavior:SetSpeedMultiplier(self.path_speed_multiplier)
+    self.actor.behaviors.ChasePlayerBehavior:SetMaxChaseTime(45000) -- max time spent pathing towards the target before we get the timeout event (in milliseconds)
+
+    self:DeclareNetworkSubscriptions()
+end
+
+function CityLooter:DeclareNetworkSubscriptions()
+    Network:Subscribe("npc/PlayerAttackNPC" .. tostring(self.actor.actor_id), self, self.PlayerAttacked)
 end
 
 function CityLooter:GetSyncData()
@@ -23,6 +52,7 @@ function CityLooter:GetSyncData()
         sync_data.path_speed_multiplier = self.path_speed_multiplier
     end
     sync_data.position = self.actor:GetPosition()
+    --sync_data.shooting = 
     
     return sync_data
 end
@@ -34,8 +64,15 @@ function CityLooter:Initialize(data)
     end
     
     self.actor.behaviors.RoamBehavior:FindNextPath() -- self.actor.position should be up-to-date before calling FindNextPath
+
+    self:DeclareNetworkSubscriptions()
 end
 
+function CityLooter:GetLineOfSightOffset()
+    return Vector3()
+end
+
+-- "PathAcquired" behavior event handler from ChasePlayerBehavior
 function CityLooter:PathAcquired(path)
     if self.removed then return end
     if not self.announced then
@@ -63,7 +100,86 @@ end
 function CityLooter:PathFinished()
     self.actor.position = Copy(self.path.positions[#self.path.positions])
     self.actor.cell = GetCell(self.actor.position, ActorSync.cell_size)
+
+    if self:IsChasing() then
+        self.actor.behaviors.ShootTargetBehavior:StopShootingTarget(false)
+        self.actor.behaviors.ChasePlayerBehavior:StartChasing(self.target) -- self.actor.position should be up-to-date before calling StartChasing
+    else
+        self.actor.behaviors.RoamBehavior:FindNextPath() -- self.actor.position should be up-to-date before calling FindNextPath
+    end
+end
+
+function CityLooter:PlayerAttacked(args, player)
+    local should_acquire_target = false
+    if self.target ~= player then
+        should_acquire_target = true
+    elseif not self.target or not IsValid(self.target) then
+        should_acquire_target = true
+    end
+    if not self.actor:IsPlayerStreamedIn(player) then
+        print("tried to set NewTarget but new target is not streamed in")
+        should_acquire_target = false
+    end
+
+    --print("should_acquire_target: ", should_acquire_target)
+    
+    if should_acquire_target then
+        self:NewTarget(player)
+        self.actor.behaviors.NavigatePathBehavior:Pause()
+        if args.line_of_sight then
+            self.actor.behaviors.ChasePlayerBehavior:StopChasing()
+            self.actor.behaviors.ShootTargetBehavior:ShootTarget(self.target)
+        else
+            self:UpdateStoredPosition()
+            self.actor.behaviors.ChasePlayerBehavior:StartChasing(player)
+        end
+    end
+
+    self.actor:SyncStateEventsIfNecessary()
+end
+
+function CityLooter:NewTarget(new_target)
+    self.target = new_target
+end
+
+function CityLooter:StoppedShootingTarget() -- happens when LoS is lost after min duration of shooting
+    self:UpdateStoredPosition()
+    if self.target and IsValid(self.target) then
+        self.actor.behaviors.ShootTargetBehavior:StopShootingTarget(false)
+        self.actor.behaviors.ChasePlayerBehavior:StartChasing(self.target)
+    else
+        self.target = nil
+        self.actor.behaviors.RoamBehavior:FindNextPath()
+    end
+    --Chat:Broadcast("Entered StoppedShootingTarget", Color.Red)
+    self.actor:SyncStateEventsIfNecessary()
+end
+
+function CityLooter:LineOfSightGainedWhenChasing()
+    self.actor.behaviors.NavigatePathBehavior:Pause()
+    self.actor.behaviors.ChasePlayerBehavior:StopChasing()
+    self.actor.behaviors.ShootTargetBehavior:ShootTarget(self.target)
+    self.actor:SyncStateEventsIfNecessary()
+end
+
+-- from ChasePlayerBehavior
+function CityLooter:ChasingTimedOut()
+    self.target = nil
+    self:UpdateStoredPosition()
     self.actor.behaviors.RoamBehavior:FindNextPath() -- self.actor.position should be up-to-date before calling FindNextPath
+    self.actor.behaviors.NavigatePathBehavior:Pause()
+    self.actor.behaviors.ChasePlayerBehavior:StopChasing()
+
+    self.actor:SyncStateEventsIfNecessary()
+end
+
+function CityLooter:IsChasing()
+    return self.actor.behaviors.ChasePlayerBehavior.chasing
+end
+
+function CityLooter:UpdateStoredPosition()
+    self.actor.position = self.actor.behaviors.NavigatePathBehavior:GetPosition()
+    self.actor.cell = GetCell(self.actor.position, ActorSync.cell_size)
 end
 
 function CityLooter:Remove()
