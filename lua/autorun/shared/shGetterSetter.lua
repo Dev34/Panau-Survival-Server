4954f194994dee2b82fd0787f82924189da36c17
Rainier Devolder <devolde2@msu.edu>
2020-09-23 22:06:33 -0400

    use lua addition split for getter setter

--- lua/autorun/shared/shGetterSetter.lua
+++ lua/autorun/shared/shGetterSetter.lua
@@ -6,7 +6,7 @@ function getter_setter(instance, get_set_name)
     end
 
     local name = ""
-    local words = get_set_name:split("_")
+    local words = split(get_set_name, "_")
     for k, word in ipairs(words) do
         name = name .. firstCharacterToUpper(word)
     end
